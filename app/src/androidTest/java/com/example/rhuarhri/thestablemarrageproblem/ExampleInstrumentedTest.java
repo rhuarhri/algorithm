38575c10d7ccb882c135dacafa3448c8fdce6e34
rhuarhri <rhuarhri@gmail.com>
2019-05-09 12:42:38 +0100

    UI can now interact with the algorithm with automated test for this interaction.

--- app/src/androidTest/java/com/example/rhuarhri/thestablemarrageproblem/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/rhuarhri/thestablemarrageproblem/ExampleInstrumentedTest.java
@@ -1,12 +1,35 @@
 package com.example.rhuarhri.thestablemarrageproblem;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+//import android.support.test.InstrumentationRegistry;
+//import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnitRunner;
 
+import android.graphics.Point;
+import android.os.RemoteException;
+import android.view.View;
+
+
+import androidx.test.espresso.Espresso;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +39,53 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.rhuarhri.thestablemarrageproblem", appContext.getPackageName());
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void UITest()
+    {
+
+        //project 1
+        String projectName = "text to speech app";
+        List<String> projectTopics = new ArrayList<String>();
+        //topics in order of importance
+        projectTopics.add("natural language processing");
+        projectTopics.add("app development");
+        projectTopics.add("UI design");
+
+        addProject(projectName, projectTopics);
+
+        //project 2
+        projectName = "home smart lock";
+        projectTopics = new ArrayList<String>();
+        //topics in order of importance
+        projectTopics.add("IOT");
+        projectTopics.add("cyber security");
+        projectTopics.add("app development");
+
+        addProject(projectName, projectTopics);
+
+        String expected = "\nProject text to speech app is paired with  lecturer DR Brown.\n"+
+                            "Project home smart lock is paired with  lecturer DR Robinson.";
+        onView(withId(R.id.resultsDisplayTXT)).check(matches(withText(expected)));
+
+    }
+
+    private void addProject(String name, List<String> topics)
+    {
+        onView(withId(R.id.projectNameET)).perform(typeText(name), closeSoftKeyboard());
+
+        for (int i = 0; i < topics.size(); i++)
+        {
+            onView(withId(R.id.projectTopicsET)).perform(typeText(topics.get(i)), closeSoftKeyboard());
+            onView(withId(R.id.addTopicBTN)).perform(click());
+        }
+
+        onView(withId(R.id.saveProjectBTN)).perform(click());
     }
 }
